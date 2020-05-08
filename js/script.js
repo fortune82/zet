16c3d202bde0884aadc30bad7131a2afacf8a952
fortuna8.2 <fortuna8228@gmail.com>
2020-05-09 01:27:22 +0300

    меню

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let burgerMenu = document.querySelector(".burgerMenu");
+let menu = document.querySelector(".menu");
+burgerMenu.onclick = () => {
+    if (menu.style.display = "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
